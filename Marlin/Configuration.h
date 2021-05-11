252c3714a66c0369c65c26aa0d1df703cbac0af0
Jan <schick.ne.email@gmail.com>
2021-05-11 09:14:04 +0200

    Update E-Steps
    Calibrated to my personal Machine

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -759,7 +759,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT    { 93, 93, 1097.5, 97 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT    { 93, 93, 1097.5, 108 }
 
 /**
  * Default Max Feed Rate (mm/s)
