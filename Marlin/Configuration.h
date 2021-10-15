585d47c4dd2c6ffbc76087d9a4a0c55c2558a489
ljan <schick.ne.email@gmail.com>
2021-10-15 14:47:12 +0200

    Added BMG Extruder
    Update E-Steps

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -759,7 +759,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT    { 93, 93, 1097.5, 108 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT    { 93, 93, 1097.5, 404 }
 
 /**
  * Default Max Feed Rate (mm/s)
